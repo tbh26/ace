afda482ad4d7ae6cf5d3f1cd10ca8854744e7e76
Tjeerd Hes <tjeerdh@roamer.local>
2022-11-09 23:16:24 +0100

    [ace_app_stack] initial setup

---
+++ ace_app_stack/test/ace_app_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AceAppStack from '../lib/ace_app_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ace_app_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AceAppStack.AceAppStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
